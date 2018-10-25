3655a92e437a347adffe4d1930cc90e03de6e593
lidongdong2253 <lidongdong2253@163.com>
2018-10-25 20:05:46 +0800

    11

---
+++ web-mobile/src/assets/scripts/config/config.3ffcf.js
@@ -0,0 +1,13 @@
+
+//配置
+var config = {
+    "isDebug":true,//是否是测试状态 
+    "isWeb":false,//是不是web
+    "canvasWidth":1280,//
+    "onorientationChange" : "portrait",//竖屏 "landscape"//横屏
+    // "loginUrl":"http://192.168.1.118:8116",//http登录
+    "loginUrl":"http://192.168.1.131:8116",//http登录
+    "socketUrl":"ws://127.0.0.1:8034/ws",
+} 
+
+
